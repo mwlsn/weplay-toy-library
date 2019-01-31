b7e153e5ca476f3a7e12b93b649488ed0a0e0ff6
Melanie Wilson <47125108+mwlsn@users.noreply.github.com>
2019-01-31 14:43:06 -0500

    Update content-toys.php
    Clean-up.

--- content-toys.php
+++ content-toys.php
@@ -22,8 +22,8 @@
 <br>
 <div class="toy-id"><p><?php echo get_post_meta($post->ID, 'Toy ID', true); ?></p></div>
 	<div class="toy-name">	<h1 class="entry-title"><?php print_post_title() ?></h1></div>
-		
-			
+
+
 		<?php
 	if ( has_post_thumbnail() ) { ?>
 	<figure class="featured-image toy-medium">
@@ -31,8 +31,9 @@
 		the_post_thumbnail();
 		?>
 	</figure><!-- .featured-image  -->
-	<p><a class="btn btn-primary btn-lg toy-button" href="https://search.clevnet.org/client/en_US/clevnet/search/detailnonmodal/ent:$002f$002fSD_ILS$002f0$002fSD_ILS:7574802/one" role="button">View Item Availability</a></p>
-	
+	<p><a class="btn btn-primary btn-lg toy-button" href="https://search.clevnet.org/client/en_US/clevnet/search/detailnonmodal/ent:$002f$002fSD_ILS$002f0$002fSD_ILS:7574802/one" role="button"
+		View Item Availability</a></p>
+
 	<div class="toy-info-full"><div class="taxonomies">
 			  <div class="age">
         <?php echo get_the_term_list( $post->ID, 'age', 'Age: ', ', ', '' ); ?>
@@ -53,10 +54,10 @@
 	<?php } ?>
 
 <div class="hold">
-  <p>Toys are available on a first come first serve basis. Please call the owning library to confirm the item's availability.</p> 
+  <p>Toys are available on a first come first serve basis. Please call the owning library to confirm the item's availability.</p>
 	<p><strong>Willoughby Public Library</strong>: 440-942-3200 | <strong>Willowick Public Library</strong>: 440-943-4151</p></div>
- 
-		<?php 
+
+		<?php
 			the_content();
 
 			$spacious_tag_list = get_the_tag_list( '', '&nbsp;&nbsp;&nbsp;&nbsp;', '' );
@@ -70,7 +71,7 @@
 				<?php
 			}
 
-			wp_link_pages( array( 
+			wp_link_pages( array(
 			'before'            => '<div style="clear: both;"></div><div class="pagination clearfix">'.__( 'Pages:', 'spacious' ),
 			'after'             => '</div>',
 			'link_before'       => '<span>',
@@ -78,18 +79,17 @@
       ) );
 		?>
 
-	
 
-				
+
+
 
 		</div>
 
 
  <h3><a href="http://we247.org/toys/"> < Back to List </a> </h3>
- 
+
 <hr>
 	<?php
 	do_action( 'spacious_after_post_content' );
    ?>
 </article>
-
